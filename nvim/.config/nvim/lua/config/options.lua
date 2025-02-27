4674929856f5d64f59957bd87443f3fd5c5db2b1
Yannick Spies <yannickspies91@gmail.com>
2025-02-27 09:57:12 +0100

    updated emmet-ls

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,29 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Neovide specific settings
+if vim.g.neovide then
+  -- Font
+  vim.o.guifont = "IntoneMono NF:h15"
+
+  -- Padding
+  vim.g.neovide_padding_top = 10
+  vim.g.neovide_padding_bottom = 10
+  vim.g.neovide_padding_right = 10
+  vim.g.neovide_padding_left = 10
+
+  -- Transparency
+  vim.g.neovide_transparency = 0.8
+
+  -- No animations
+  vim.g.neovide_cursor_animation_length = 0
+
+  -- Other recommended settings
+  vim.g.neovide_refresh_rate = 60
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_confirm_quit = true
+
+  -- Since you're using WSL, you might want to add:
+  vim.g.neovide_remember_window_size = true
+end
--- nvim/.config/nvim/lua/plugins/emmet-ls.lua
+++ nvim/.config/nvim/lua/plugins/emmet-ls.lua
@@ -1,33 +1,43 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        emmet_ls = {
-          filetypes = {
-            "css",
-            "eruby",
-            "html",
-            "javascript",
-            "javascriptreact",
-            "less",
-            "sass",
-            "scss",
-            "svelte",
-            "pug",
-            "typescriptreact",
-            "vue",
-          },
-          init_options = {
-            html = {
-              options = {
-                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-                ["bem.enabled"] = true,
-              },
+    opts = function(_, opts)
+      -- Setup capabilities with snippet support
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      -- Configure emmet-ls
+      opts.servers = opts.servers or {}
+      opts.servers.emmet_ls = {
+        capabilities = capabilities,
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "svelte",
+          "pug",
+          "typescriptreact",
+          "vue",
+          -- Additional filetypes from default config
+          "astro",
+          "htmldjango",
+          "htmlangular",
+        },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
             },
           },
         },
-      },
-    },
+      }
+      return opts
+    end,
   },
 }
