fc14635497af2385286c4aa052f90ff7a9930eee
Yannick Spies <yannickspies91@gmail.com>
2025-02-28 21:02:47 +0100

    added emmet-ls, oil

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
