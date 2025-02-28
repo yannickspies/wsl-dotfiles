e269b7242e18785218e70cdf6bb5d0ec882d7416
Yannick Spies <yannickspies91@gmail.com>
2025-02-28 21:06:57 +0100

    added keymaps and disabled neotree in favour of oil

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Oil
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Select all text
+map("n", "<C-a>", "gg<S-v>G", { desc = "Select all text" })
+
+-- Disable continuations
+map("n", "<Leader>o", "o<Esc>^Da", opts)
+map("n", "<Leader>O", "O<Esc>^Da", opts)
