2753d462cca5d7f58e096252f256db5009538373
Yannick Spies <yannickspies91@gmail.com>
2025-02-28 15:42:49 +0100

    removed colorscheme

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,3 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "echasnovski/mini.nvim", enabled = false },
-  { "echasnovski/mini.ai", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-  { "echasnovski/mini.icons", enabled = false },
 }
